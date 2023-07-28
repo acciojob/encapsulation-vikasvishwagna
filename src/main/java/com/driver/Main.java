a899cba9d44b834155ff7c6685a08712901b599f
sai vikas <vishwagnavikassai@gmail.com>
2023-07-28 23:05:06 +0530

    commit encapsule two

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.setName("sahana");
+        obj.setName("vikas");
         obj.getName();
     }
   
